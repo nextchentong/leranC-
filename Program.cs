56fd4b311a7b0753833ba0cb7391e5237ac53a19
nextchentong <chentong19950106@gmail.com>
2023-02-22 10:14:53 +0800

    test

--- Program.cs
+++ Program.cs
@@ -155,7 +155,7 @@ partial class Program
         Console.WriteLine(newData);
 
 
-        String connetStr = "server=127.0.0.1;port=3306;user=root;password=123456; database=test;";
+        String connetStr = "server=121.4.251.168;port=3306;user=root;password=123456; database=test;";
         MySqlConnection conn = new MySqlConnection(connetStr);
 
         try
